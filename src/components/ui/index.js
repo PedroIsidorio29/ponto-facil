b429e448222a62b191913fc52c78397fa587d1ff
Pedro Henrique Isidorio de Souza <pedroisidorio1@gmail.com>
2025-08-17 16:07:46 -0300

    feature: initial structure

---
+++ src/components/ui/index.js
@@ -0,0 +1,4 @@
+export { default as Button } from "@mui/material/Button";
+export { default as TextField } from "@mui/material/TextField";
+export { default as Box } from "@mui/material/Box";
+export { default as Typography } from "@mui/material/Typography";
